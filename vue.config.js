10740ad43b99d63e985276845e27ffc03cce750e
Virgel007 <neokrium.88@gmail.com>
2024-10-31 18:50:56 +0300

    local_install v1.002

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   devServer: {
-    proxy: 'http://localhost:8080',
-    // proxy: 'http://82.202.214.42:8899/',
+    // proxy: 'http://localhost:8080',
+    proxy: 'http://79.174.80.223:8080'
     //  proxy: 'http://212.22.70.159:1337/',
     // proxy: 'http://localhost:8080/',
   },
