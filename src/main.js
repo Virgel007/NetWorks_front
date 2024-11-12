da147c3a0de2cf08c9d6774c496f5e29a2cdd08b
Virgel007 <neokrium.88@gmail.com>
2024-11-12 13:59:18 +0300

    local_install v1.013

--- src/main.js
+++ src/main.js
@@ -36,8 +36,8 @@ app
   .use(router)
   .use(store)
   .use(chat, {
-    // server: 'http://79.174.80.223:8085'
-    server: 'localhost:8080'
+    server: '79.174.80.223'
+    // server: 'localhost:8080'
   })
   .use(vClickOutside)
   .use(useVuelidate)
