78de779b2f289334dbc35db2cf28ab3c1378e18e
Virgel007 <neokrium.88@gmail.com>
2024-11-13 15:37:26 +0300

    local_install v1.015

--- src/plugins/websocket.js
+++ src/plugins/websocket.js
@@ -3,8 +3,8 @@ export default {
     let socket;
     const socketApi = {
       connect() {
-        const serverUrl = 'ws://79.174.80.223:8088/api/v1/streaming/ws';
-        // const serverUrl = 'ws://79.174.80.223:8077/api/v1/streaming/ws';
+        // const serverUrl = 'ws://79.174.80.223:8088/api/v1/streaming/ws';
+        const serverUrl = 'ws://79.174.80.223:8077/api/v1/streaming/ws';
         console.log(serverUrl);
         socket = new WebSocket(serverUrl);
         console.log('function connect');
